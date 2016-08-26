f708deac9fa41505ba9e5a969f57276ed5eaa554
wangjuneng <wangjuneng@126.com>
2016-08-26 15:37:33 +0800

    service learn

---
+++ srv-core/src/main/java/com/ztesoft/zsmart/remoting/core/common/ServiceThread.java
@@ -0,0 +1,22 @@
+package com.ztesoft.zsmart.remoting.core.common;
+
+/**
+ * 
+ * <Description> Base class for background thread<br> 
+ *  
+ * @author wang.jun<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2016年8月26日 <br>
+ * @since V7.3<br>
+ * @see com.ztesoft.zsmart.remoting.core.common <br>
+ */
+public abstract class ServiceThread implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
