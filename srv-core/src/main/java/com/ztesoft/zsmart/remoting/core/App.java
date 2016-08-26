66b1d21428efe54b11b545d442751bf81c1fa682
wangjuneng <wangjuneng@126.com>
2016-08-26 10:06:37 +0800

    srv-core

---
+++ srv-core/src/main/java/com/ztesoft/zsmart/remoting/core/App.java
@@ -0,0 +1,13 @@
+package com.ztesoft.zsmart.remoting.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
